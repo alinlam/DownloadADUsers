eb03f471e6b4e9b649412f84db917ceccb00c3e0
alin <lmlien@gmail.com>
2020-06-18 16:35:26 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace ADUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This console application will download all AD accounts and the properties");
+            Console.WriteLine("Please input following information");
+            
+            Console.Write("LDAP path to the OU: ");
+            var ldap = Console.ReadLine();
+
+            Console.Write("Username: ");
+            var username = Console.ReadLine();
+
+            Console.Write("Password: ");
+            var password= Console.ReadLine();
+
+            Console.WriteLine("Start downloading process...");
+
+            var service = new Services(ldap, username, password);
+            var users = service.GetUsers();
+
+            var now = DateTime.Now;
+            var fileName = $"allUsers_{now.Year}{now.Month}{now.Day}{now.Hour}{now.Minute}{now.Second}.json";
+            //FileStream fs = new FileStream(fileName, FileMode.Create);
+            
+            using (StreamWriter sw = new StreamWriter(@$"{fileName}", true))
+            {
+                //Console.SetOut(sw);
+
+                //Console.WriteLine("[");
+                sw.WriteLine("[");
+
+                var lastUser = users.Last();
+                foreach (var user in users)
+                {
+                    //Console.WriteLine("\t{");
+                    //Console.WriteLine($"\t\"UserPrincipalName\": \"{user.UserPrincipalName}\",");
+                    //Console.WriteLine($"\t\"Properties\":");
+                    //Console.WriteLine("\t\t[");
+
+                    sw.WriteLine("\t{");
+                    sw.WriteLine($"\t\"UserPrincipalName\": \"{user.UserPrincipalName}\",");
+                    sw.WriteLine($"\t\"Properties\":");
+                    sw.WriteLine("\t\t[");
+
+                    var lastProp = user.Properties.Last();
+                    foreach (var prop in user.Properties)
+                    {
+                        if (prop.Key == lastProp.Key)
+                        {
+                            //Console.WriteLine($"\t\t\t{JsonConvert.SerializeObject(prop)}");
+                            sw.WriteLine($"\t\t\t{JsonConvert.SerializeObject(prop)}");
+                        }
+                        else
+                        {
+                            //Console.WriteLine($"\t\t\t{JsonConvert.SerializeObject(prop)},");
+                            sw.WriteLine($"\t\t\t{JsonConvert.SerializeObject(prop)},");
+                        }
+                    }
+
+                    //Console.WriteLine("\t\t]");
+                    sw.WriteLine("\t\t]");
+
+                    if (user.UserPrincipalName == lastUser.UserPrincipalName)
+                    {
+                        //Console.WriteLine("\t}");
+                        sw.WriteLine("\t}");
+                    }
+                    else
+                    {
+                        //Console.WriteLine("\t},");
+                        sw.WriteLine("\t},");
+                    }
+
+                }
+                //Console.WriteLine("]");
+                sw.WriteLine("]");
+            }           
+
+            //TextWriter tmp = Console.Out;
+            //Console.SetOut(tmp);
+            Console.WriteLine($"Completed downloaded {users.Count()} users, data is saved in file {fileName}");
+
+            Console.WriteLine("Press any key to exit the program");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Services.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace ADUsers
+{
+    public class ADUser
+    {
+        public string UserPrincipalName { get; }
+
+        public Dictionary<string, object> Properties { get; }
+
+        public ADUser(string userPrincipalName, Dictionary<string, object> properties)
+        {
+            this.Properties = properties;
+            this.UserPrincipalName = userPrincipalName;
+        }
+    }
+
+    public class Services
+    {
+        private string _ldap;
+        private string _username;
+        private string _password;
+        public Services(string ldap, string username, string password)
+        {
+            this._ldap = ldap;
+            this._username = username;
+            this._password = password;
+        }
+        public IEnumerable<ADUser> GetUsers()
+        {
+            int pageNumber = 1;
+            int pageSize = 100;
+            var results = new List<ADUser>();
+
+            while (true)
+            {
+                Console.WriteLine($"Getting {pageSize} users of page #{pageNumber}");
+
+                var skipRows = (pageNumber - 1) * pageSize;
+                var users = this.GetUsersInOU(this._ldap, this._username, this._password, pageSize, skipRows, (skipRows + pageSize));
+                if (users.Count == 0)
+                {
+                    break;
+                }
+
+                results.AddRange(users);
+                Console.WriteLine($"Got {users.Count} users of page #{pageNumber}, up the total to {results.Count} records");
+
+                if (users.Count < pageSize)
+                {
+                    Console.WriteLine($"Finished downloading process");
+                    break;
+                }
+
+                // Continue next page
+                pageNumber++;
+            }
+
+            return results;
+        }
+
+        private List<ADUser> GetUsersInOU(string ldap, string username, string password, int pageSize, int fromIndex, int toIndex)
+        {
+            Console.WriteLine($"Accessing {ldap}");
+
+            string filter = $"(&(objectCategory=person)(objectClass=user)(|(givenName=*)(sn=*)))";
+            var usersFound = new List<ADUser>();
+            
+
+            using (DirectoryEntry dirEntry = new DirectoryEntry(ldap, username, password))
+            {
+                using (DirectorySearcher dirSearcher = new DirectorySearcher(dirEntry, filter, null))
+                {
+                    dirSearcher.Sort = new SortOption("cn", SortDirection.Ascending);
+                    dirSearcher.PageSize = pageSize;
+                    dirSearcher.SearchScope = SearchScope.Subtree;
+                    dirSearcher.PropertiesToLoad.Add(string.Empty);                    
+
+                    using (var searchResults = dirSearcher.FindAll())
+                    {
+                        for (int i = fromIndex; i < toIndex; i++)
+                        {
+                            SearchResult result = null;
+                            try
+                            {
+                                result = searchResults[i];
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex);
+                                break;
+                            }
+
+                            using (var de = result.GetDirectoryEntry())
+                            {
+                                var properties = new Dictionary<string, object>();
+                                foreach (string key in de.Properties.PropertyNames)
+                                {
+                                    properties.Add(key, de.Properties[key]);
+                                }
+
+                                var userPrincipalName = (de.Properties["userPrincipalName"] as PropertyValueCollection).Value as string;
+
+                                usersFound.Add(new ADUser(userPrincipalName, properties));
+
+                                Console.WriteLine($"Got Directory Entry for {userPrincipalName}, found: {properties.Count} properties");
+                            }
+                        }                        
+                    }
+
+                    dirEntry.Close();
+                    dirEntry.Dispose();
+                }
+            }
+
+            return usersFound;
+        }
+    }
+}
